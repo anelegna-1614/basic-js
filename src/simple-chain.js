4626e2ac26619d0338951e2226a678f050779332
anelegna-1614 <anelegna-1614@tut.by>
2021-03-29 21:22:02 +0300

    feat: add solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,31 @@
 const CustomError = require("../extensions/custom-error");
-
+ let chain;
 const chainMaker = {
+   result: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.result.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.result.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if ((Number.isInteger(position) || Number.isNaN(this.result[position])) && position <= this.result.length){
+      this.result.splice(position -1, 1);
+      return this;
+    } else {
+      this.result = [];
+      throw new Error;
+    }  
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.result.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    chain = this.result;
+    this.result = [];
+    return chain.join('~~');
   }
 };
 
